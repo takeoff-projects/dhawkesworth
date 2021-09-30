e632deafed4527fcaf5c43a439dc68880ff09c89
David Hawkesworth <david.hawkesworth@takeoff.com>
2021-09-30 14:58:57 -0400

    Adjusts for Terraform files being in terraform dir

--- start_my_app.sh
+++ start_my_app.sh
@@ -1,4 +1,5 @@
-cd terraform
-gcloud builds submit -t gcr.io/$DEVSHELL_PROJECT_ID/go-pets
+gcloud builds submit -t gcr.io/$DEVSHELL_PROJECT_ID/go-pets:v1.0 .
+pushd terraform
 terraform init
 terraform apply -auto-approve
+popd
